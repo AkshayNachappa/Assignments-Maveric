a196b6ba69ce790ae88d06e9f1101cc39d4c0414
Akshay Nachappa <akshaynachappa@gmail.com>
2021-04-07 18:15:22 +0530

    del old folder

--- Web/Assignment-5/README.md
+++
@@ -1,2 +0,0 @@
-Open 'Organized' folder 'As Project' in IDE  
-File Structure according to IntelliJ
--- Web/Assignment-5/script.js
+++
@@ -1,5 +0,0 @@
-function clear(){
-    alert("Filters Cleared");
-    document.getElementById("state-options").options.length="";
-    document.getElementById("category-options").value="";
-}
