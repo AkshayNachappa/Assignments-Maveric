15c8f23d01b61627eca39ce579741ae0b6d37203
Akshay Nachappa <akshaynachappa@gmail.com>
2021-04-15 10:18:44 +0530

    created galleryView

---
+++ Assessment-1-Qs/GalleyView/scripts.js
@@ -0,0 +1,16 @@
+/*
+const buttonRight = document.getElementById('slideRight');
+const buttonLeft = document.getElementById('slideLeft');
+var ele = document.getElementById('container');
+buttonRight.onclick = function () {
+    ele.scrollLeft += 50;
+};
+buttonLeft.onclick = function () {
+    ele.scrollLeft -= 20;
+};
+*/
+
+function disp(x){
+    res.value = x;
+    
+}
