22af2cc6382d99ac07e9e50478f00e29cbb2f619
Akshay Nachappa <akshaynachappa@gmail.com>
2021-04-17 22:56:25 +0530

    main-branch-mgmt

--- Practice/Assessment-1-Qs/GalleryView/scripts.js
+++ Practice/Assessment-1-Qs/GalleryView/scripts.js
@@ -1,10 +1,10 @@
 // Part A starts
-function scrLt(){
-    document.getElementById('container').scrollLeft -= 30;
-}
-function scrRt(){
-    document.getElementById('container').scrollLeft += 30;
-}
+// function scrLt(){
+//     document.getElementById('container').scrollLeft -= 30;
+// }
+// function scrRt(){
+//     document.getElementById('container').scrollLeft += 30;
+// }
 // Part A ends
 
 function disp(x){
