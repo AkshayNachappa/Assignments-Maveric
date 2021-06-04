8065f69f006a25a88a9b59872b2ac98e37f84e01
Akshay Nachappa <akshaynachappa@gmail.com>
2021-06-05 00:56:05 +0530

    modify to suit q-structure

--- Practice/Assessment-1-Qs/GalleryView/app.js
+++ Practice/Assessment-1-Qs/GalleryView/app.js
@@ -1,23 +1,27 @@
         var items_list = [0,1,2,3,4,5,6]
         var gallery_windowsize = 3;
         function moveLeft(){
-            items_list.push(items_list.shift())
+            var x = items_list.shift();
+            items_list.push(x) 
             document.getElementById("list_a").innerHTML=items_list[0];
             document.getElementById("list_b").innerHTML=items_list[1];
             document.getElementById("list_c").innerHTML=items_list[2];
+            document.getElementById("displayText").innerHTML = "None Selected"
         }
 
         function moveRight(){
-            items_list.unshift(items_list.pop())
+
+            items_list.unshift(items_list.pop()) //6,0,1,2,3,4,5
             document.getElementById("list_a").innerHTML=items_list[0];
             document.getElementById("list_b").innerHTML=items_list[1];
             document.getElementById("list_c").innerHTML=items_list[2];
+            document.getElementById("displayText").innerHTML = "None Selected"
 
 
         }
 
         function displaySelect(id){
-            document.getElementById("displayText").innerHTML = document.getElementById(id).innerHTML;
+            document.getElementById("displayText").innerHTML = "You Have Selected "+document.getElementById(id).innerHTML+" element";
 
         }
         
--- Practice/Assessment-1-Qs/Light Bulbs/scripts.js
+++ Practice/Assessment-1-Qs/Light Bulbs/scripts.js
@@ -1,13 +1,25 @@
-function toggle(){
-    var bulb = document.getElementById("bulb");
-    var res = document.getElementById("res");
-    if(bulb.src.match("bulbOn")){
-        bulb.src = "images/bulbOff.jpg";
-        res.innerHTML = "Bulb Off"
-
+function onOff1(){
+    var bulb1 = document.getElementById("left");
+    var bulb2 = document.getElementById("right");
+    if(bulb1.src.match("99929e1")){
+        bulb1.src = "Insert src of the ON bulb";
+        bulb2.src = "Insert src of the OFF bulb";
     }
     else{
-        bulb.src = "images/bulbOn.jpg"
-        res.innerHTML = "Bulb On"
+        bulb1.src = "Insert src of the OFF bulb"
+        bulb2.src = "Insert src of the ON bulb"  
     }
+}
+function onOff2(){
+    var bulb1 = document.getElementById("left");
+    var bulb2 = document.getElementById("right");
+    if(bulb2.src.match("99929e1")){
+        bulb2.src = "Insert src of the ON bulb";
+        bulb1.src = "Insert src of the OFF bulb";
+    }
+    else{
+        bulb2.src = "Insert src of the OFF bulb"
+        bulb1.src = "Insert src of the ON bulb"  
+    }
+
 }
